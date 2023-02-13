d47546af3c92f86de72025f05592c28835297085
Michaelszeng <michaelszeng@gmail.com>
2023-02-13 15:11:21 -0500

    workshop 1 finished

--- workshop-1/Button/Button.ino
+++ workshop-1/Button/Button.ino
@@ -2,31 +2,24 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
+  pinMode(7, INPUT_PULLUP);
 
   Serial.begin(9600);
   Serial.println("Setup Complete.");
 }
 
 // TODO: EXPLAIN THIS LATER
-//int led_brightness = 0
+int led_brightness = 0
 
 void loop() {
   // put your main code here, to run repeatedly:  
+  int button = digitalRead(7);
 
-// COMMENT THIS OUT AND IMPLEMENT ABOVE
-//  int led_brightness = 0
-  
-  // Make LED brighter
-  for (int i = 0; i < 255; i += 1) {
-    analogWrite(13, led_brightness);
-    delay(50);
+  if (button == LOW) {
     led_brightness += 1;
   }
-
-  // Make LED dimmer
-  for (int i = 0; i < 255; i += 1) {
-    analogWrite(13, led_brightness);
-    delay(50);
+  else {
     led_brightness -= 1;
   }
+  analogWrite(13, led_brightness);
 }
